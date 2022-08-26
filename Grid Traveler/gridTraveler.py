1a3bdb28eb0cdcd1cb0e8037af6b2020445c06a7
Sajib Mandal <107785806+sajib-mandal@users.noreply.github.com>
2022-08-26 14:42:55 +0530

    Update gridTraveler.py

--- Grid Traveler/gridTraveler.py
+++ Grid Traveler/gridTraveler.py
@@ -1,4 +1,25 @@
-19 lines (15 sloc)  448 Bytes
+"""
+Say that you ara a traveler on a 2D grid. You begin in the
+top-left corner and your goal is to travel to the bottom-right
+corner. You may only move down or right.
+
+In how many ways can you travel to the goal on a grid with
+dimension m * n.
+"""
+
+# Recursive without Memoization
+def gridTraveler(m, n):
+    if m == 1 and n == 1:
+        return 1
+    if m == 0 or n == 0:
+        return 0
+    return gridTraveler(m - 1, n) + gridTraveler(m, n - 1)
+
+print(gridTraveler(1, 1))
+print(gridTraveler(2, 3))
+print(gridTraveler(3, 2))
+print(gridTraveler(3, 3))
+print(gridTraveler(18, 18)) # Don't try
 
 # Recursive with Memoization
 
