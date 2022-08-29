4e264dba6543cb7208e3fef49e87f9c4cda189ca
Sajib Mandal <107785806+sajib-mandal@users.noreply.github.com>
2022-08-29 15:06:58 +0530

    Create how_sum.py

---
+++ Memoization and Recursion/how_sum.py
@@ -0,0 +1,23 @@
+def how_sum(target_sum: int, numbers: list, memo={}) -> list:
+    if target_sum in memo:
+        return memo[target_sum]
+    if target_sum == 0:
+        return []
+    if target_sum < 0:
+        return None
+    for num in numbers:
+        remainder = target_sum - num
+        remainder_res = how_sum(remainder, numbers, memo)
+        if remainder_res is not None:
+            remainder_res.append(num)
+            memo[target_sum] = remainder_res
+            return memo[target_sum]
+
+    memo[target_sum] = None
+
+
+print(how_sum(7, [2, 3], memo={}))
+print(how_sum(7, [5, 3, 4, 7], memo={}))
+print(how_sum(7, [2, 4], memo={}))
+print(how_sum(8, [2, 3, 5], memo={}))
+print(how_sum(300, [7, 14], memo={}))
