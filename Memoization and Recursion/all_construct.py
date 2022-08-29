b6c0bcb07d5a37c2dfd927a396e6df27cda9e98b
Sajib Mandal <107785806+sajib-mandal@users.noreply.github.com>
2022-08-29 23:48:00 +0530

    Create all_construct.py

---
+++ Memoization and Recursion/all_construct.py
@@ -0,0 +1,20 @@
+# recursion without memoization
+
+def all_construct(target: str, word_bank: list) -> list:
+    if target == '':
+        return [[]]
+    result = []
+    for word in word_bank:
+        if target.startswith(word):
+            suffix = target[len(word):]
+            suffix_ways = all_construct(suffix, word_bank)
+            target_ways = [el[:] for el in suffix_ways]
+            [el.insert(0, word) for el in target_ways]
+            result.extend(target_ways)
+    return result
+
+
+print(all_construct('purple', ['purp', 'p', 'ur', 'le', 'purpl']))
+print(all_construct('abcdef', ['ab', 'abc', 'cd', 'def', 'abcd', 'ef', 'c']))
+print(all_construct('skateboard', ['bo', 'rd', 'ate', 't', 'ska', 'sk', 'bord']))
+print(all_construct('aaaaaaaaaaaaaaaaaaaaaaaaaaz', ['a', 'aa', 'aaa', 'aaaa', 'aaaaa']))
