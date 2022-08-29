a1cb02226004986105e16dc53feb2645749c0a18
Sajib Mandal <107785806+sajib-mandal@users.noreply.github.com>
2022-08-29 22:30:40 +0530

    Create count_construct.py

---
+++ Memoization and Recursion/count_construct.py
@@ -0,0 +1,20 @@
+# recursive without memoization
+
+def count_construct(target: str, word_bank: list) -> int:
+    if target == '':
+        return 1
+    total_count = 0
+    for word in word_bank:
+        if target.startswith(word):
+            suffix = target[len(word):]
+            num_ways_for_rest = count_construct(suffix, word_bank)
+            total_count += num_ways_for_rest
+
+    return total_count
+
+
+print(count_construct('purple', ['purp', 'p', 'ur', 'le', 'purpl']))  # 2
+print(count_construct('abcdef', ['ab', 'abc', 'cd', 'def', 'abcd']))  # 1
+print(count_construct('skateboard', ['bo', 'rd', 'ate', 't', 'ska', 'sk', 'boar']))  # 0
+print(count_construct('enterapotentpot', ['a', 'p', 'ent', 'enter', 'ot', 'o', 't']))  # 4
+print(count_construct('eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef', ['e', 'ee', 'eee', 'eeee', 'eeeee', 'eeeeee']))
