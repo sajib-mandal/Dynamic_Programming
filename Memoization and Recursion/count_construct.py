ecc0801d951aedfc0b8be6d767242e46845ece04
Sajib Mandal <107785806+sajib-mandal@users.noreply.github.com>
2022-08-29 22:36:26 +0530

    Update count_construct.py

--- Memoization and Recursion/count_construct.py
+++ Memoization and Recursion/count_construct.py
@@ -18,3 +18,27 @@ print(count_construct('abcdef', ['ab', 'abc', 'cd', 'def', 'abcd']))  # 1
 print(count_construct('skateboard', ['bo', 'rd', 'ate', 't', 'ska', 'sk', 'boar']))  # 0
 print(count_construct('enterapotentpot', ['a', 'p', 'ent', 'enter', 'ot', 'o', 't']))  # 4
 print(count_construct('eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef', ['e', 'ee', 'eee', 'eeee', 'eeeee', 'eeeeee']))
+
+
+# recursion with memoization
+
+def count_construct(target: str, word_bank: list, memo={}) -> int:
+    if target in memo:
+        return memo[target]
+    if target == '':
+        return 1
+    total_count = 0
+    for word in word_bank:
+        if target.startswith(word):
+            suffix = target[len(word):]
+            num_ways_for_rest = count_construct(suffix, word_bank, memo)
+            total_count += num_ways_for_rest
+    memo[target] = total_count
+    return total_count
+
+
+print(count_construct('purple', ['purp', 'p', 'ur', 'le', 'purpl'], memo={}))  # 2
+print(count_construct('abcdef', ['ab', 'abc', 'cd', 'def', 'abcd'], memo={}))  # 1
+print(count_construct('skateboard', ['bo', 'rd', 'ate', 't', 'ska', 'sk', 'boar'], memo={}))  # 0
+print(count_construct('enterapotentpot', ['a', 'p', 'ent', 'enter', 'ot', 'o', 't'], memo={}))  # 4
+print(count_construct('eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef', ['e', 'ee', 'eee', 'eeee', 'eeeee', 'eeeeee'], memo={}))
