a0de92c379acbd01d7b5f6c2f2eee9c9979e0af0
Sajib Mandal <107785806+sajib-mandal@users.noreply.github.com>
2022-09-03 22:55:43 +0530

    Update min_change.py

--- Memoization and Recursion/min_change.py
+++ Memoization and Recursion/min_change.py
@@ -16,3 +16,37 @@ def min_change(amount, coins):
 print(min_change(8, [1, 5, 4, 12]))
 print(min_change(13, [1, 9, 5, 14, 30]))
 print(min_change(300, [9, 7, 3]))
+
+
+
+
+# recursion with memoization
+
+def min_change(amount, coins):
+    ans = _min_change(amount, coins, {})
+    if ans == float("inf"):
+        return -1
+    else:
+        return ans
+
+
+def _min_change(amount, coins, memo):
+    if amount in memo:
+        return memo[amount]
+    if amount < 0:
+        return float("inf")
+    if amount == 0:
+        return 0
+
+    min_coins = float("inf")
+    for coin in coins:
+        num_coins = 1 + _min_change(amount - coin, coins, memo)
+        if num_coins < min_coins:
+            min_coins = num_coins
+    memo[amount] = min_coins
+    return min_coins
+
+print(min_change(8, [1, 5, 4, 12]))
+print(min_change(13, [1, 9, 5, 14, 30]))
+print(min_change(300, [9, 7, 3]))
+print(min_change(2017, [4, 2, 10]))
