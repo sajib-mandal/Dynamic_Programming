9eaab3ddbd7b34fda2f201b5ff7b62344f0f66bd
Sajib Mandal <107785806+sajib-mandal@users.noreply.github.com>
2022-09-04 16:48:47 +0530

    Update summing_squares.py

--- Memoization and Recursion/summing_squares.py
+++ Memoization and Recursion/summing_squares.py
@@ -24,5 +24,38 @@ def summing_squares(n):
 
 print(summing_squares(8))  # -> 2
 print(summing_squares(9))  # -> 1
-print(summing_squares(10))  # ->
+print(summing_squares(10))  # -> 2
 print(summing_squares(12))  # -> 3
+
+
+
+# recursive with memoization
+
+import math
+
+def summing_squares(n):
+    return _summing_squares(n, {})
+
+def _summing_squares(n, memo):
+    if n in memo:
+        return memo[n]
+    if n == 0:
+        return 0
+    min_squares = float("inf")
+    for i in range(1, math.floor(math.sqrt(n)) + 1):
+        square = i * i
+        num_squares = 1 + _summing_squares(n - square, memo)
+        min_squares = min(num_squares, min_squares)
+        memo[n] = min_squares
+    return min_squares
+
+
+print(summing_squares(8))  # -> 2
+print(summing_squares(9))  # -> 1
+print(summing_squares(10))  # -> 2
+print(summing_squares(12))  # -> 3
+print(summing_squares(1))  # -> 1
+print(summing_squares(31))  # -> 4
+print(summing_squares(50))  # -> 2
+print(summing_squares(68))  # -> 2
+print(summing_squares(87))  # -> 4
