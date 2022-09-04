99dff6625ee70d8c34eec1631479a929502cf747
Sajib Mandal <107785806+sajib-mandal@users.noreply.github.com>
2022-09-04 12:41:26 +0530

    Update count_paths.py

--- Memoization and Recursion/count_paths.py
+++ Memoization and Recursion/count_paths.py
@@ -4,7 +4,7 @@ def count_paths(grid):
     return _count_paths(grid, 0, 0)
 
 def _count_paths(grid, r, c):
-    if r == len(grid) or c == len(grid[0]) or grid[r][c] == "X":
+    if r == len(grid) or c == len(grid[0]) or grid[r][c] == "X":   # c == len(grid[0]) is inner grid : its means just pust the right broder.
         return 0
     if r == len(grid) - 1 and c == len(grid[0]) - 1:
         return 1
@@ -28,7 +28,7 @@ def _count_paths(grid, r, c, memo):
     pos = (r, c)
     if pos in memo:
         return memo[pos]
-    if r == len(grid) or c == len(grid[0]) or grid[r][c] == "X":
+    if r == len(grid) or c == len(grid[0]) or grid[r][c] == "X":    # c == len(grid[0]) is inner grid : its means just pust the right broder.
         return 0
     if r == len(grid) - 1 and c == len(grid[0]) - 1:
         return 1
