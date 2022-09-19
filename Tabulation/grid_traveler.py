31dd6dc459b42e6bb6d29d1573910ea7f58e3da3
Sajib Mandal <107785806+sajib-mandal@users.noreply.github.com>
2022-09-19 22:45:43 +0530

    Update grid_traveler.py

--- Tabulation/grid_traveler.py
+++ Tabulation/grid_traveler.py
@@ -17,3 +17,23 @@ print(grid_traveler(2, 3))
 print(grid_traveler(3, 2))
 print(grid_traveler(3, 3))
 print(grid_traveler(18, 18))
+
+
+
+def grid_traveler(m, n):
+    if 1 == m or 1 == n:
+        return 1
+
+    dp = [[1 for j in range(n)] for i in range(m)]
+
+    for y in range(1, m):
+        for x in range(1, n):
+            dp[y][x] = dp[y - 1][x] + dp[y][x - 1]
+
+    return dp[-1][-1]
+
+print(grid_traveler(1, 1))
+print(grid_traveler(2, 3))
+print(grid_traveler(3, 2))
+print(grid_traveler(3, 3))
+print(grid_traveler(18, 18))
